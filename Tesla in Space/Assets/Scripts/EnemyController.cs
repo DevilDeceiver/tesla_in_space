9db8d36aa2879a773231418aacf6eafa9deecd01
vfilipovi <46088355+vfilipovi@users.noreply.github.com>
2018-12-23 22:31:17 +0100

    dodan player,enemy,pucanje, kolizije iz nekog razloga ne rade

---
+++ Tesla in Space/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float zivoti = 20;
+    public float brzina = 0.5f;
+
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0,
+            -brzina) * 1;
+
+        rb2d.angularVelocity = Random.Range(-200, 200);
+    }
+
+    void OnBecameInvisible()
+    {
+        // Destroy the enemy
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+
+
+}
---
+++ Tesla in Space/Assets/Scripts/ProjektilSkripta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjektilSkripta : MonoBehaviour
+{
+    public float brzina = 5;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        // Make the bullet move upward
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0,
+            brzina) * 1;
+        //original
+        // = new Vector2(horizontal,  0) *brzina;
+    }
+    
+    // Function called when the object goes out of the screen
+    void OnBecameInvisible()
+    {
+        // Destroy the bullet 
+        Destroy(gameObject);
+    }
+}
